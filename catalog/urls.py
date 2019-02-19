c58627782046061020e588f9ae946c57a4001f0c
khashiboush <khashayar.boush@icloud.com>
2019-02-19 15:03:31 -0500

    full Work

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,8 +6,8 @@ app_name='catalog'
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
     path('books/', views.BookListView.as_view(), name='books'),
-    path('book/<int : pk>/', views.BookDetailView.as_view(), name='book-detail'),
+    path('book/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),
     path('authors/', views.AuthorListView.as_view(), name='authors'),
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
-    path('book/<int : pk>/renew/', views.renew_book_librarian, name='renew-book-librarian')
+    path('book/<int:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian')
 ]
