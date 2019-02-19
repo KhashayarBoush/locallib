8f423e32e51da2ed9ed4a861011791be2efdd9b8
khashiboush <khashayar.boush@icloud.com>
2019-02-19 14:48:20 -0500

    url to path convert

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -13,17 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.urls import path, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^catalog/', include('catalog.urls')),
+    path('admin/', admin.site.urls),
+    path('catalog/', include('catalog.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += [
-    url(r'^accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
